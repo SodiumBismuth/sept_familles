ae9dd61b807d1b981fc96a996d98bf7251b1f460
Nathan BIZON <90388021+SodiumBismuth@users.noreply.github.com>
2021-12-16 18:36:36 +0000

    Finished

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 Présentation de la famille de l'IA
 
 |Travail|Fait par|TAF|
-|:------------------|:--------------|:---------------------------|
-|Modification du CSS|Nathan le 29/11|Police d'écriture + couleurs|
-|finition de la description des personnages|Tout le monde le 30/11|images + recherches sur les personnages|
-|début du quiz|tout le monde le 30/11|recherche des questions|
-|continuation du quiz|js : Nathan //   html : Robin et Mael // le 14/12|transcriptions des questions en python + js|
-|finition des questions et du projet|tout le monde le 16/12|finition de la mise en page + vérification du programme|
+|:-----------------------------------------|:------------------------------------------------|:------------------------------------------------------|
+|Modification du CSS                       |Nathan le 29/11|Police d'écriture + couleurs     |                                                       |
+|finition de la description des personnages|Tout le monde le 30/11                           |images + recherches sur les personnages                |
+|début du quiz                             |tout le monde le 30/11                           |recherche des questions                                |
+|continuation du quiz                      |js : Nathan //   html : Robin et Mael // le 14/12|transcriptions des questions en python + js            |
+|finition des questions et du projet       |tout le monde le 16/12                           |finition de la mise en page + vérification du programme|
 ||||
--- quizz.js
+++ quizz.js
@@ -1,46 +1,59 @@
-function sendanswers():
-	let n = 0
-	if document.getElementById("v1").checked {
-		document.getElementById("RQ1") = "Bonne réponse !"
+document.getElementById('REP').onclick = function sendanswers(){
+	let n = 0;
+	if (document.getElementById("v1").checked) {
+		document.getElementById("RQ1").innerHTML = "Bonne réponse !";
+		document.getElementById("RQ1").style = "color:green";
 		n++
 	}
 	else {
-		document.getElementById("RQ1") = "Mauvaise réponse..."
+		document.getElementById("RQ1").innerHTML = "Mauvaise réponse...";
+		document.getElementById("RQ1").style = "color:darkred"
 	}
-	if document.getElementById("v2").checked {
-		document.getElementById("RQ2") = "Bonne réponse !"
+	if (document.getElementById("v2").checked) {
+		document.getElementById("RQ2").innerHTML = "Bonne réponse !";
+		document.getElementById("RQ2").style = "color:green";
 		n++
 	}
 	else {
-		document.getElementById("RQ2") = "Mauvaise réponse..."
+		document.getElementById("RQ2").innerHTML = "Mauvaise réponse...";
+		document.getElementById("RQ2").style = "color:darkred"
 	}
-	if document.getElementById("Q3").selectedIndex == 2 {
-		document.getElementById("RQ3") = "Bonne réponse !"
+	if (document.getElementById("Q3").selectedIndex == 2) {
+		document.getElementById("RQ3").innerHTML = "Bonne réponse !";
+		document.getElementById("RQ3").style = "color:green";
 		n++
 	}
 	else {
-		document.getElementById("RQ3") = "Mauvaise réponse..."
+		document.getElementById("RQ3").innerHTML = "Mauvaise réponse...";
+		document.getElementById("RQ3").style = "color:darkred"
 	}
-	if document.getElementById("Q4").selectedIndex == 5 {
-		document.getElementById("RQ4") = "Bonne réponse !"
+	if (document.getElementById("Q4").selectedIndex == 5) {
+		document.getElementById("RQ4").innerHTML = "Bonne réponse !";
+		document.getElementById("RQ4").style = "color:green";
 		n++
 	}
 	else {
-		document.getElementById("RQ4") = "Mauvaise réponse..."
+		document.getElementById("RQ4").innerHTML = "Mauvaise réponse...";
+		document.getElementById("RQ4").style = "color:darkred"
 	}
-	if document.getElementById(""). {
-		document.getElementById("RQ5") = "Bonne réponse !"
+	if (document.getElementById("Q5").value == "1960") {
+		document.getElementById("RQ5").innerHTML = "Bonne réponse !";
+		document.getElementById("RQ5").style = "color:green";
 		n++
 	}
 	else {
-		document.getElementById("RQ5") = "Mauvaise réponse..."
+		document.getElementById("RQ5").innerHTML = "Mauvaise réponse...";
+		document.getElementById("RQ5").style = "color:darkred"
 	}
-	if document.getElementById("2").checked and document.getElementById("4").checked {
-		document.getElementById("RQ6") = "Bonne réponse !"
+	if (document.getElementById("2").checked && document.getElementById("4").checked) {
+		document.getElementById("RQ6").innerHTML = "Bonne réponse !";
+		document.getElementById("RQ6").style = "color:green";
 		n++
 	}
 	else {
-		document.getElementById("RQ6") = "Mauvaise réponse..."
+		document.getElementById("RQ6").innerHTML = "Mauvaise réponse...";
+		document.getElementById("RQ6").style = "color:darkred"
 	}
-	document.getElementById("NBR") = "Vous avez " + n + " bonnes réponses sur 6 !"
-	
+	document.getElementById("NBR").innerHTML = "Vous avez " + n + " bonnes réponses sur 6 !"
+}
+
